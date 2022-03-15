0c10f1cbf66823ae13b15099805e863cac80fc73
SmissTake <lil.smiss7@gmail.com>
2022-03-15 14:24:41 +0100

    Graphical user interface

---
+++ app/src/main/java/com/example/gsb/Departement.java
@@ -0,0 +1,13 @@
+package com.example.gsb;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Departement extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.departement);
+    }
+}
---
+++ app/src/main/java/com/example/gsb/RechercheNom.java
@@ -0,0 +1,13 @@
+package com.example.gsb;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RechercheNom extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recherche_nom);
+    }
+}
